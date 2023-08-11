0037f84664afbd6601bf17465f530e97db8fd293
addoumi mohammed <addoumi.mohammed@gmail.com>
2023-08-11 20:46:02 +0100

    init project

---
+++ src/main/java/com/backreview/backreview/BackReviewApplication.java
@@ -0,0 +1,13 @@
+package com.backreview.backreview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackReviewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackReviewApplication.class, args);
+	}
+
+}
